65c2e1a7b2e6f20cb2418e7a496109f7fdc10f39
Mason Schmidgall <mason.schmidgall1@gmail.com>
2024-11-17 22:08:24 -0600

    add quoting functionality

--- src/bot.py
+++ src/bot.py
@@ -3,6 +3,30 @@ import discord
 
 TOKEN = os.getenv('SPICYBOT_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print("Tree sync complete")
+
+@tree.command(
+    name='quote',
+    description='Make a quote of a user in the designated quotes channel'
+)
+@discord.app_commands.describe(
+    user='The user you are quoting',
+    quote='The bars they be spittin'
+)
+async def quote_cmd(interaction: discord.Interaction, user: discord.User, quote: str):
+    quotes_channel: discord.TextChannel = discord.utils.get(
+        interaction.guild.channels, name='quotes'
+    )
+    await quotes_channel.send(f'"{quote}"\n-{user.mention}')
+    await interaction.response.send_message('Success (probably)!')
 
 client.run(TOKEN)
